a9383e8a2e772f260bb21b5fb478de790c43789c
lilu <lilulucas.li@qunar.com>
2019-08-14 11:40:57 +0800

    导航默认导航进行base64加密

--- QIMCommon/Source/QIMNavConfig/QIMNavConfigManager.m
+++ QIMCommon/Source/QIMNavConfig/QIMNavConfigManager.m
@@ -75,12 +75,14 @@
 
         if ([[QIMAppInfo sharedInstance] appType] == QIMProjectTypeQTalk) {
             NSString *qtalkSettingsPath = [[NSBundle mainBundle] pathForResource:@"qtalkDefaultSetting" ofType:@"json"];
-            NSData *data = [[NSData alloc] initWithContentsOfFile:qtalkSettingsPath];
-            self.defaultSettings = [[QIMJSONSerializer sharedInstance] deserializeObject:data error:nil];
+            NSString *str = [[NSString alloc] initWithContentsOfFile:qtalkSettingsPath];
+            NSData *encodeBase64Data = [NSData qim_dataWithBase64EncodedString:str];
+            self.defaultSettings = [[QIMJSONSerializer sharedInstance] deserializeObject:encodeBase64Data error:nil];
         } else if ([[QIMAppInfo sharedInstance] appType] == QIMProjectTypeQChat) {
             NSString *qchatSettings = [[NSBundle mainBundle] pathForResource:@"qchatDefaultSetting" ofType:@"json"];
-            NSData *data = [[NSData alloc] initWithContentsOfFile:qchatSettings];
-            self.defaultSettings = [[QIMJSONSerializer sharedInstance] deserializeObject:data error:nil];
+            NSString *str = [[NSString alloc] initWithContentsOfFile:qchatSettings];
+            NSData *encodeBase64Data = [NSData qim_dataWithBase64EncodedString:str];
+            self.defaultSettings = [[QIMJSONSerializer sharedInstance] deserializeObject:encodeBase64Data error:nil];
         } else {
             self.defaultSettings = nil;
         }
