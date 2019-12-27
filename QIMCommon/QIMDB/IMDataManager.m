90dc978a4f9df0425b55b9af0fba66db27445bb6
froyomu <lihaibin.li@qunar.com>
2019-12-27 16:30:53 +0800

    fix qchat login bug;

--- QIMCommon/QIMDB/IMDataManager.m
+++ QIMCommon/QIMDB/IMDataManager.m
@@ -132,7 +132,7 @@ static dispatch_once_t _onceDBToken;
 }
 
 - (NSInteger)qim_dbVersion {
-    return 5;
+    return 6;
 }
 
 - (void)updateDBVersionToFileWithVersion:(NSInteger)upgradeResultVersion {
@@ -178,6 +178,11 @@ static dispatch_once_t _onceDBToken;
         case 4: {
             result = [self upgradeFrom4To5];
             currentOldVersion = 4;
+        }
+            break;
+        case 5: {
+            result = [self upgradeFrom5To6];
+            currentOldVersion = 5;
         }
             break;
         default: {
@@ -313,6 +318,19 @@ static dispatch_once_t _onceDBToken;
                   end" ];
         
         result = [database executeNonQuery:@"delete from logs" withParameters:nil];
+    }];
+    return result;
+}
+
+- (BOOL)upgradeFrom5To6 {
+    QIMVerboseLog(@"upgradeFrom5To6");
+    __block BOOL result = YES;
+    [_databasePool inDatabase:^(QIMDataBase* _Nonnull database) {
+        if ([database columnExists:@"IM_TRIP_INFO" columnName:@"tripCode"] == NO) {
+            result = [database executeNonQuery:@"ALTER TABLE IM_TRIP_INFO ADD tripCode TEXT;" withParameters:nil];
+        } else {
+            result = YES;
+        }
     }];
     return result;
 }
@@ -910,7 +928,8 @@ static dispatch_once_t _onceDBToken;
               tripRoomNumber TEXT,\
               memberList TEXT,\
               tripRemark TEXT,\
-              canceled Text);" ];
+              canceled TEXT,\
+              tripCode TEXT);" ];
     
     //创建log表
     result = [database executeUpdate:@"CREATE TABLE IF NOT EXISTS logs (\
